4b7a2da736b1d842f7cf29b229b7162994ce6736
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-10-13 20:54:48 +0700

    Menambahkan fungsi main untuk menggunakan DataClass dan demonstrasi fungsi equals dan copy

--- app/src/main/java/com/example/app/DataClass.kt
+++ app/src/main/java/com/example/app/DataClass.kt
@@ -9,3 +9,20 @@ data class DataClass(
     val id : Int,
     val email: String
 )
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) //false
+
+    // fungsi copy
+    val data3 = data.copy()
+    println(data3)
+
+    // fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
