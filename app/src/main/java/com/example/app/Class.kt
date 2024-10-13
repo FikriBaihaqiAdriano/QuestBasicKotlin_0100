e2bc43c816853176efeda6815afa046303ae4ca8
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-10-13 17:03:09 +0700

    Menambahkan println untuk menampilkan email dari objek Contact

--- app/src/main/java/com/example/app/Class.kt
+++ app/src/main/java/com/example/app/Class.kt
@@ -6,4 +6,9 @@ class Contact(val id: Int, var email: String)
 
 fun main() {
 val contact = Contact(1, "mary@gmail.com")
+
+// Prints the value of the property : email
+    println(contact.email)
+
+
 }
