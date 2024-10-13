9732cf7ea844dba7512b0ec325b9c4d1e4ee6f69
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-10-13 20:30:49 +0700

    Membuat if else agar mengetahui angka positif atau angka negatif

--- app/src/main/java/com/example/app/ConditionalExpression.kt
+++ app/src/main/java/com/example/app/ConditionalExpression.kt
@@ -6,3 +6,13 @@ package com.example.app
 
 fun ConditionalStatement() {
     println("=== Conditional Expression ====")
+
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+}
